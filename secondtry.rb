4885dd1b709101f1232b3115f7d6bdc4cad3f42f
“scottasc” <scottschulfer@gmail.com>
2018-08-20 18:51:46 -0500

    added puts

--- secondtry.rb
+++ secondtry.rb
@@ -1,5 +1,5 @@
-Hello.
-They gave me a tin hat.
-They sent me on off to the war.
-To Turkey. They sent me to Turkey.
-To Gallipolli.
+puts Hello.
+puts They gave me a tin hat.
+puts They sent me on off to the war.
+puts To Turkey. They sent me to Turkey.
+puts To Gallipolli.
